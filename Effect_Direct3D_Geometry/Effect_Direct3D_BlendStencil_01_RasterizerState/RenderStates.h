210dffc87a94ee8916947448b39e4a8439092561
Kevin04261004 <kdystudy0426@gmail.com>
2024-11-04 17:26:33 +0900

    Geometry

---
+++ Effect_Direct3D_Geometry/Effect_Direct3D_BlendStencil_01_RasterizerState/RenderStates.h
@@ -0,0 +1,28 @@
+//***************************************************************************************
+// RenderStates.h by Frank Luna (C) 2011 All Rights Reserved.
+//   
+// Defines render state objects.  
+//***************************************************************************************
+
+#ifndef RENDERSTATES_H
+#define RENDERSTATES_H
+
+#include "d3dUtil.h"
+
+class RenderStates
+{
+public:
+	static void InitAll(ID3D11Device* device);
+	static void DestroyAll();
+
+	static ID3D11RasterizerState* WireframeRS;
+	static ID3D11RasterizerState* NoCullWireframeRS;
+	static ID3D11RasterizerState* NoCullRS;
+	static ID3D11RasterizerState* CullRS;
+
+	 
+	static ID3D11BlendState* AlphaToCoverageBS;
+	static ID3D11BlendState* TransparentBS;
+};
+
+#endif // RENDERSTATES_H
